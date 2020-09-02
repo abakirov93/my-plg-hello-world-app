26b6bf84d363b3611d7a99e6e21672229eab4c33
None <abakirov93@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-02 18:32:10 +0000

    added build job

---
+++ JenkinsBuilder.groovy
@@ -0,0 +1,3 @@
+@Library('CommonLib@master') _
+def common = new com.lib.JenkinsCommonDockerBuildPipeline()
+common.runPipeline()
---
+++ deployments/docker/app.py
@@ -0,0 +1,7 @@
+from flask import Flask,  jsonify 
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return jsonify({'message': 'hello world'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
